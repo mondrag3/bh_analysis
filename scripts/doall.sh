e260b906b472de391f9945df4df8a5418d8534c3
ivankp <ivan.pogrebnyak@gmail.com>
2014-11-18 23:04:25 -0500

    doall

--- scripts/doall.sh
+++ scripts/doall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-data=
+data=~/raida/data
 
 ./bin/hist_weights $data/Wm1j7TeV_born_0.weights.root config/all_weights.bins $data/Wm1j7TeV_born_0.new_hist_uncut.root
 
 ./bin/select_old_weight_hists -i $data/Wm1j7TeV_born_0.HIST.root -o $data/Wm1j7TeV_born_0.old_hist_uncut.root
 
-./bin/draw_together -o cmp_lin.pdf -i old:data/Wm1j7TeV_born_0.old_hist_uncut.root -i new:data/Wm1j7TeV_born_0.new_hist_uncut_small.root
+./bin/draw_together -o cmp_lin.pdf -i old:$data/Wm1j7TeV_born_0.old_hist_uncut.root -i new:$data/Wm1j7TeV_born_0.new_hist_uncut.root
 
-./bin/draw_together -o cmp_logx.pdf --logx -i old:data/Wm1j7TeV_born_0.old_hist_uncut.root -i new:data/Wm1j7TeV_born_0.new_hist_uncut_small.root
+./bin/draw_together -o cmp_logx.pdf --logx -i old:$data/Wm1j7TeV_born_0.old_hist_uncut.root -i new:$data/Wm1j7TeV_born_0.new_hist_uncut.root
