c2ab6e52e580c5d005bb90526fc319fea78d5ca3
ivankp <ivan.pogrebnyak@gmail.com>
2014-11-25 16:44:13 -0500

    improving drawing

--- scripts/doall.sh
+++ scripts/doall.sh
@@ -5,10 +5,10 @@ data=data
 
 part=$1
 
+if false; then
+
 echo -e "\n\E[0;49;93mReweighting\E[0m\n"
 ./bin/reweigh --bh=$data/Wm1j7TeV_"$part"_0.BH.root -o $data/Wm1j7TeV_"$part"_0.weights.root --pdf=CT10 --old-bh
-
-#if false; then
 
 echo -e "\n\E[0;49;93mMaking new histograms\E[0m\n"
 ./bin/hist_weights $data/Wm1j7TeV_"$part"_0.weights.root config/all_weights.bins $data/Wm1j7TeV_"$part"_0.new_hist_uncut.root
@@ -16,9 +16,14 @@ echo -e "\n\E[0;49;93mMaking new histograms\E[0m\n"
 echo -e "\n\E[0;49;93mSelecting old histograms\E[0m\n"
 ./bin/select_old_weight_hists -i $data/Wm1j7TeV_"$part"_0.HIST.root -o $data/Wm1j7TeV_"$part"_0.old_hist_uncut.root --pdf=CT10 --uncut
 
+fi
+
 echo -e "\n\E[0;49;93mDrawing comparison plots\E[0m\n"
-./bin/draw_together -o cmp_"$part"_lin.pdf -i old:$data/Wm1j7TeV_"$part"_0.old_hist_uncut.root -i new:$data/Wm1j7TeV_"$part"_0.new_hist_uncut.root --name-title
+./bin/draw_together -o out/cmp_"$part"_lin.pdf -i old:$data/Wm1j7TeV_"$part"_0.old_hist_uncut.root -i new:$data/Wm1j7TeV_"$part"_0.new_hist_uncut.root --name-title
 
-./bin/draw_together -o cmp_"$part"_logx.pdf --logx -i old:$data/Wm1j7TeV_"$part"_0.old_hist_uncut.root -i new:$data/Wm1j7TeV_"$part"_0.new_hist_uncut.root --name-title
+./bin/draw_together -o out/cmp_"$part"_logx.pdf --logx -i old:$data/Wm1j7TeV_"$part"_0.old_hist_uncut.root -i new:$data/Wm1j7TeV_"$part"_0.new_hist_uncut.root --name-title
 
-#fi
+./bin/draw_together -o out/cmp_"$part"_logy.pdf --logy -i old:$data/Wm1j7TeV_"$part"_0.old_hist_uncut.root -i new:$data/Wm1j7TeV_"$part"_0.new_hist_uncut.root --name-title
+
+./bin/draw_together -o out/cmp_"$part"_logxy.pdf --logx --logy -i old:$data/Wm1j7TeV_"$part"_0.old_hist_uncut.root -i new:$data/Wm1j7TeV_"$part"_0.new_hist_uncut.root --name-title
+
