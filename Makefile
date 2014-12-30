5b3fb1e2fc89fe21681aa16a260972ddf14e2f86
ivankp <ivan.pogrebnyak@gmail.com>
2014-12-30 00:12:11 -0500

    src/test_fj_H3j.cc

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ LHAPDF_LIBS   := $(shell lhapdf-config --ldflags)
 .PHONY: all misc clean
 
 # bin/test_rew_calc
-EXE := bin/reweigh bin/hist_H2j bin/test_H3j bin/plot
+EXE := bin/reweigh bin/hist_H2j bin/plot bin/test_H3j bin/test_fj_H3j
 
 all: $(DIRS) $(EXE)
 
@@ -34,7 +34,7 @@ lib/rew_calc.o: lib/%.o: src/%.cc src/%.h
 	@$(CPP) $(CFLAGS) $(ROOT_CFLAGS) $(LHAPDF_CFLAGS) -c $(filter %.cc,$^) -o $@
 
 # main object rules
-lib/cross_section_bh.o lib/cross_section_hist.o lib/test_rew_calc.o lib/reweigh.o lib/hist_weights.o lib/select_old_weight_hists.o lib/draw_together.o lib/plot.o lib/test_H3j.o: lib/%.o: src/%.cc
+lib/cross_section_bh.o lib/cross_section_hist.o lib/test_rew_calc.o lib/reweigh.o lib/hist_weights.o lib/select_old_weight_hists.o lib/draw_together.o lib/plot.o lib/test_H3j.o lib/test_fj_H3j.o: lib/%.o: src/%.cc
 	@echo -e "Compiling \E[0;49;94m"$@"\E[0;0m ... "
 	@$(CPP) $(CFLAGS) $(ROOT_CFLAGS) -c $(filter %.cc,$^) -o $@
 
@@ -68,6 +68,10 @@ bin/draw_together bin/test_H3j: bin/%: lib/%.o
 	@echo -e "Linking \E[0;49;92m"$@"\E[0;0m ... "
 	@$(CPP) $(filter %.o,$^) -o $@ $(ROOT_LIBS) -lboost_program_options
 
+bin/test_fj_H3j: bin/%: lib/%.o
+	@echo -e "Linking \E[0;49;92m"$@"\E[0;0m ... "
+	@$(CPP) $(filter %.o,$^) -o $@ $(ROOT_LIBS) -lm -lfastjettools -lfastjet
+
 bin/hist_H2j bin/hist_H3j: bin/%: lib/%.o
 	@echo -e "Linking \E[0;49;92m"$@"\E[0;0m ... "
 	@$(CPP) -Wl,--no-as-needed $(filter %.o,$^) -o $@ $(ROOT_LIBS) -lboost_program_options -lboost_regex -lkiwihist
@@ -84,14 +88,14 @@ lib/cross_section_bh.o: src/BHEvent.h
 lib/test_rew_calc.o: src/rew_calc.h src/BHEvent.h
 lib/reweigh.o      : src/rew_calc.h src/BHEvent.h src/timed_counter.h
 lib/hist_H2j.o lib/hist_H3j.o : src/BHEvent.h src/SJClusterAlg.h src/finder.h src/timed_counter.h
-lib/test_H3j.o : src/BHEvent.h src/SJClusterAlg.h src/timed_counter.h
+lib/test_H3j.o lib/test_fj_H3j.o : src/BHEvent.h src/SJClusterAlg.h src/timed_counter.h
 lib/plot.o         : src/propmap11.h
 
 # EXE dependencies
 bin/cross_section_bh: lib/BHEvent.o
 bin/test_rew_calc  : lib/rew_calc.o lib/BHEvent.o
 bin/reweigh        : lib/rew_calc.o lib/BHEvent.o
-bin/hist_H2j bin/hist_H3j bin/test_H3j : lib/BHEvent.o lib/SJClusterAlg.o
+bin/hist_H2j bin/hist_H3j bin/test_H3j bin/test_fj_H3j : lib/BHEvent.o lib/SJClusterAlg.o
 
 clean:
 	rm -rf bin lib
---
+++ src/test_fj_H3j.cc
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <array>
+#include <cmath>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TChain.h>
+#include <TLorentzVector.h>
+
+#include <fastjet/ClusterSequence.hh>
+
+#include "BHEvent.h"
+// #include "SJClusterAlg.h"
+// #include "finder.h"
+#include "timed_counter.h"
+
+#define test(var) \
+  cout <<"\033[36m"<< #var <<"\033[0m"<< " = " << var << endl;
+
+using namespace std;
+using namespace fastjet;
+
+// template<typename T> inline T sq(const T& x) { return x*x; }
+
+int main(int argc, char** argv)
+{
+  if (argc!=3) {
+    cout << "Usage: " << argv[0] << " bh.root wt.root" << endl;
+    exit(0);
+  }
+
+  // Setup input files **********************************************
+  TChain*    tree = new TChain("t3");
+  TChain* wt_tree = new TChain("weights");
+
+  // Add trees from all the files to the TChains
+  if (!   tree->AddFile(argv[1],-1) ) exit(1);
+  if (!wt_tree->AddFile(argv[2],-1) ) exit(1);
+
+  // Friend BlackHat tree with SpartyJet and Weight trees
+  tree->AddFriend(wt_tree,"weights");
+
+  // BlackHat tree branches
+  BHEvent event;
+  event.SetTree(tree, BHEvent::kinematics);
+
+  tuple<string,Double_t,Double_t> _weight;
+  array<tuple<string,Float_t,Double_t>,3> weight;
+  get<0>(weight[0]) = "Fac0.25Ht_Ren0.25Ht_PDFCT10nlo_cent";
+  get<0>(weight[1]) = "Fac0.5Ht_Ren0.5Ht_PDFCT10nlo_cent";
+  get<0>(weight[2]) = "Fac1Ht_Ren1Ht_PDFCT10nlo_cent";
+  tree->SetBranchAddress( get<0>(_weight).c_str(),
+                         &get<1>(_weight) );
+  for (short i=0;i<3;++i)
+    tree->SetBranchAddress( get<0>(weight[i]).c_str(),
+                           &get<1>(weight[i]) );
+
+  // Choose Clustering Algorithm
+  const JetDefinition jet_def(antikt_algorithm, 0.4);
+  cout << "\nClustering with " << jet_def.description() << endl << endl;
+
+  // Reading events from the input TChain ***************************
+  const Long64_t nent = tree->GetEntries();
+  cout << "Reading " << nent << " entries" << endl;
+  timed_counter counter;
+
+  Long64_t selected = 0;
+
+  for (Long64_t ent = 0; ent < nent; ++ent) {
+    counter(ent);
+    tree->GetEntry(ent);
+
+    // Collect Final State particles
+    vector<PseudoJet> particles;
+    particles.reserve(event.nparticle);
+
+    for (Int_t i=0; i<event.nparticle; ++i) {
+      if (event.kf[i]==25) continue;
+      particles.emplace_back(
+        event.px[i],event.py[i],event.pz[i],event.E[i]
+      );
+    }
+
+    // Perform clustering
+    ClusterSequence cs(particles, jet_def);
+
+    // Sort jets by Pt
+    const vector<PseudoJet> jets = sorted_by_pt(cs.inclusive_jets());
+    short njets = 0;
+    for (auto& j : jets) {
+      if (sqrt(j.kt2()) < 30.) continue;
+      if (abs(j.rapidity()) > 4.4) continue;
+      ++njets;
+    }
+
+    if (njets>=3) {
+      get<2>(_weight) += get<1>(_weight);
+      for (short i=0;i<3;++i)
+        get<2>(weight[i]) += get<1>(weight[i]);
+      ++selected;
+    }
+  }
+
+  counter.prt(nent);
+  cout << endl << endl;
+
+  cout << "Accepted " << selected << " of " << nent << " events" << endl;
+  cout << "Ntuple weight" << endl;
+  cout << "σ = "
+       << showpoint << setprecision(6) << get<2>(_weight)/nent
+       << " pb" << endl;
+  for (auto& w : weight) {
+    cout << get<0>(w) << endl;
+    cout << "σ = "
+         << showpoint << setprecision(6) << get<2>(w)/nent
+         << " pb" << endl;
+  }
+
+  delete tree;
+  delete wt_tree;
+
+  return 0;
+}
