50465724838dac0dde0848bb43d6fa7f8b5a6745
ivankp <ivan.pogrebnyak@gmail.com>
2015-02-19 20:20:12 -0600

    overlay with labels

--- src/overlay.cc
+++ src/overlay.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <list>
@@ -13,9 +15,10 @@
 #include <TCanvas.h>
 #include <TStyle.h>
 #include <TLegend.h>
+#include <TLine.h>
 // #include <TText.h>
 // #include <TLatex.h>
-// #include <TAxis.h>
+#include <TAxis.h>
 
 #include "propmap.hh"
 #include "hist_range.hh"
@@ -48,7 +51,7 @@ unordered_map<prop_ptr,string> rm_eq_fields(const vector<prop_ptr>& ss, const ch
     }
   }
   //cout << endl;
-  
+
   unordered_map<prop_ptr,string> _map;
   for (size_t i=0; i<n; ++i) {
     string& _s = _map[ss[i]];
@@ -59,12 +62,32 @@ unordered_map<prop_ptr,string> rm_eq_fields(const vector<prop_ptr>& ss, const ch
     }
     //cout << _s << endl;
   }
-  
+
   return _map;
 };
 
+template<class T>
+inline T* get(TDirectory* dir, const char* name) {
+  T *obj = nullptr;
+  dir->GetObject(name,obj);
+  if (obj) return obj;
+  else {
+    cerr << "No object " << name << " in " << dir->GetName() << endl;
+    exit(1);
+  }
+}
+
+string sigma_prt(Double_t sigma, unsigned prec) {
+  stringstream ss;
+  ss << "#sigma = " << showpoint << setprecision(prec) << sigma
+     << noshowpoint << " pb";
+  return ss.str();
+}
+
+enum class var : char { other, N, pT, mass, y, phi, HT, tau };
+
 // ******************************************************************
-typedef propmap<TH1*,2> hmap_t;
+typedef propmap<TH1D*,2> hmap_t;
 typedef hmap_t::key_t hkey_t;
 
 constexpr Color_t color[] = { 2, 3, 4, 6, 7, 8, 9, 28, 30, 46 };
@@ -93,6 +116,12 @@ int main(int argc, char **argv)
 
   TFile *f = new TFile(fin.c_str(),"read");
 
+  // Number of events
+  TH1D *h_N = get<TH1D>(f,"N");
+  const Double_t N_events = h_N->GetAt(1);
+  const Double_t N_scale  = 1./N_events;
+  cout << "Events: " << lround(h_N->GetEntries()) << endl;
+
   { // Collect histograms
     TIter nextkey_d(f->GetListOfKeys());
     TKey *key_d;
@@ -105,8 +134,8 @@ int main(int argc, char **argv)
           TKey *key_h;
           while ((key_h = static_cast<TKey*>(nextkey_h()))) {
             TObject *obj_h = key_h->ReadObj();
-            if (obj_h->InheritsFrom(TH1::Class())) {
-              TH1 *h = static_cast<TH1*>(obj_h);
+            if (obj_h->InheritsFrom(TH1D::Class())) {
+              TH1D *h = static_cast<TH1D*>(obj_h);
 
               hkey[0] = new prop<string>(dir->GetName());
               hkey[1] = new prop<string>(h->GetName());
@@ -118,38 +147,67 @@ int main(int argc, char **argv)
       }
     }
   }
-  
+
   // remove fields identical for all
   unordered_map<prop_ptr,string> lmap = rm_eq_fields(hmap.pvec<0>(),"_");
-  
+
   // Draw ***********************************************************
   gStyle->SetOptStat(0);
-  
+
+  TLine zero;
+  zero.SetLineStyle(7);
+
   TCanvas canv;
   canv.SaveAs((fout+'[').c_str());
 
   for (const auto& hname : hmap.pvec<1>()) {
     hkey[1] = hname;
-      
+
+    const string h_name = hname->str();
+
+    TLegend leg(0.72,0.92-0.0325*hmap.pvec<0>().size(),0.95,0.92);
+    TLegend sig(leg.GetX1(),2*leg.GetY1()-leg.GetY2(),
+                leg.GetX2(),leg.GetY1());
+    leg.SetFillColorAlpha(0,0.65);
+    sig.SetFillColorAlpha(0,0.65);
+
+    TLine top_corner_cover(leg.GetX1(),0.9,0.91,0.9);
+    TLine right_corner_cover(0.9,sig.GetY1(),0.9,0.91);
+    top_corner_cover.SetNDC();
+    right_corner_cover.SetNDC();
+    top_corner_cover.SetLineColor(0);
+    right_corner_cover.SetLineColor(0);
+    top_corner_cover.SetLineWidth(3);
+    right_corner_cover.SetLineWidth(3);
+
     hist_range y_range;
-    
-    TLegend leg(0.72,0.92-0.05*hmap.pvec<0>().size(),0.95,0.92);
-    leg.SetFillColor(0);
 
     int i = 0;
-    TH1 *first = nullptr;
+    TH1D *first = nullptr;
     for (const auto& hdir : hmap.pvec<0>()) {
       hkey[0] = hdir;
-      
-      TH1 *h;
+
+      TH1D *h;
       if (!hmap.get(hkey,h)) {
-        cerr << "Warning: No histogram " << hname->str()
+        cerr << "Warning: No histogram " << h_name
              << " in dir " << hdir->str() << endl;
         continue;
       }
-      
+
+      const Int_t nbins = h->GetNbinsX()+1;
+
+      h->Scale(N_scale);
+
+      // Cross section
+      const Double_t sigma = ( h_name.find("_N_incl") != string::npos
+                           ? h->GetAt(1)
+                           : h->Integral(0,nbins) );
+
+      for (Int_t i=0; i<nbins; ++i)
+        h->SetAt(h->GetAt(i)/(h->GetBinWidth(i)),i);
+
       y_range(h->GetMinimum(),h->GetMaximum());
-      
+
       if (i==0) {
         first = h;
         h->Draw();
@@ -158,20 +216,82 @@ int main(int argc, char **argv)
       h->SetLineWidth(2);
       h->SetLineColor(color[i]);
       h->SetMarkerColor(color[i]);
+      // h->SetFillColorAlpha(0,0);
       h->Sumw2(false);
       leg.AddEntry(h,lmap[hdir].c_str());
-      
+      sig.AddEntry(h,sigma_prt(sigma,3).c_str());
+
       ++i;
     }
 
     y_range(first)->SetTitle(hname->str().c_str());
 
+    if (first->GetMinimum()<0.)
+      zero.DrawLine(first->GetBinLowEdge(1),0,
+                    first->GetBinLowEdge(first->GetNbinsX()+1),0);
+
+    // Variable type
+    var h_var = var::other;
+
+    if (h_name.find("_N") != string::npos) {
+      h_var = var::N;
+    } else if (h_name.find("_pT") != string::npos) {
+      h_var = var::pT;
+    } else if (h_name.find("_mass") != string::npos) {
+      h_var = var::mass;
+    } else if ( (h_name.find("_deltay") != string::npos) ||
+                (h_name.find("_y") != string::npos) ) {
+      h_var = var::y;
+    } else if ( (h_name.find("_deltaphi") != string::npos) ||
+                (h_name.find("_phi") != string::npos) ) {
+      h_var = var::phi;
+    } else if (h_name.find("_HT") != string::npos) {
+      h_var = var::HT;
+    } else if (h_name.find("_tau") != string::npos) {
+      h_var = var::tau;
+    }
+
+    TAxis *xa = first->GetXaxis();
+    TAxis *ya = first->GetYaxis();
+    ya->SetTitleOffset(1.3);
+    switch (h_var) {
+      case var::pT:
+        xa->SetTitle("pT, GeV");
+        ya->SetTitle("d#sigma/dp_{T}, pb/GeV");
+        break;
+      case var::HT:
+        xa->SetTitle("HT, GeV");
+        ya->SetTitle("d#sigma/dH_{T}, pb/GeV");
+        break;
+      case var::mass:
+        xa->SetTitle("m, GeV");
+        ya->SetTitle("d#sigma/dm, pb/GeV");
+        break;
+      case var::y:
+        xa->SetTitle("y");
+        ya->SetTitle("d#sigma/dy, pb");
+        break;
+      case var::phi:
+        xa->SetTitle("#phi, rad");
+        ya->SetTitle("d#sigma/d#phi, pb/rad");
+        break;
+      case var::tau:
+        xa->SetTitle("#tau, GeV");
+        ya->SetTitle("d#sigma/d#tau, pb/GeV");
+        break;
+      default:
+        ya->SetTitle("#sigma, pb");
+    }
+
+    top_corner_cover.Draw();
+    right_corner_cover.Draw();
     leg.Draw();
+    sig.Draw();
     canv.SaveAs(fout.c_str());
   }
-  
+
   canv.SaveAs((fout+']').c_str());
-  
+
   delete f;
 
   return 0;
